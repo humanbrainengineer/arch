8a8fb49945b82845c1b1ce72c696003b592ba6f4
humanbrainengineer <apacheindex@yahoo.com>
2016-01-20 18:44:11 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -143,7 +143,7 @@ pacman -S xf86-video-vesa
 systemctl enable lxdm
 
 #Install the software：
-pacman -S sudo awesome tar vim leafpad xarchiver bcloud chromium firefox firefox-i18n-zh-cn firefox-adblock-plus flashplugin epdfview tigervnc yaourt wps-office
+pacman -S sudo ntfs-3g awesome tar vim leafpad xarchiver bcloud chromium firefox firefox-i18n-zh-cn firefox-adblock-plus flashplugin epdfview tigervnc yaourt wps-office
 
 #INstall the  typewriting :
 pacman -S fcitx fcitx-configtool fcitx-gtk2 fcitx-gtk3 fcitx-googlepinyin fcitx-qt4
