10fd70bfabd316c997f4dda5c9fd8e53d69ebbd4
Tei shimei <apacheindex@yahoo.com>
2016-02-04 16:29:09 +0800

    Update updateclean.sh

--- updateclean.sh
+++ updateclean.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+Cleaning the package cache
+
+pacman stores its downloaded packages in /var/cache/pacman/pkg/ and does not remove the old or uninstalled versions automatically, therefore it is necessary to deliberately clean up that folder periodically to prevent such folder to grow indefinitely in size.
+
+The built-in option to remove all the cached packages that are not currently installed is:
+
+# pacman -Sc
