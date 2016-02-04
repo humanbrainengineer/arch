bcf9d77a5cd851d45959b4c42ad337ccde22a352
Tei shimei <apacheindex@yahoo.com>
2016-02-04 16:29:47 +0800

    Update updateclean.sh

--- updateclean.sh
+++ updateclean.sh
@@ -6,3 +6,16 @@ pacman stores its downloaded packages in /var/cache/pacman/pkg/ and does not rem
 The built-in option to remove all the cached packages that are not currently installed is:
 
 # pacman -Sc
+
+
+
+Warning:
+
+    Only do this when certain that previous package versions are not required, for example for a later downgrade. pacman -Sc only leaves the versions of packages which are currently installed available, older versions would have to be retrieved through other means, such as the Archive.
+    It is possible to empty the cache folder fully with pacman -Scc. In addition to the above, this also also prevents from reinstalling a package directly from the cache folder in case of need, thus requiring a new download. It should be avoided unless there is an immediate need for disk space.
+
+
+
+
+
+
